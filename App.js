4a95d72c7a1f9421af1016c4629040a617abca84
Nicholas T Dimidik <nicholas.dimidik@sjsu.edu>
2022-06-13 12:53:21 -0700

    Updated App.js - Navigation working

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+//Navigation
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+//Styling
+import { NativeBaseProvider, useColorModeValue, useTheme } from 'native-base';
+import theme from './src/Theme/theme.js';
+import navigatorTheme from './src/Theme/navigatorTheme.js';
+//hooks
+import { useColorMode } from 'native-base';
+//custom components
+import TabBar from './src/components/Navigation/TabBar.js';
+//Screens
+import Home from './src/screens/home.js';
+import Grid from './src/screens/grid.js';
+import Stats from './src/screens/stats.js'
+
+
+
+const Tab = createBottomTabNavigator();
+
+
 
 export default function App() {
+
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider theme={theme}>
+      <NavigationContainer>
+        <Tab.Navigator screenOptions={{ headerShown: false,}} tabBar={props => <TabBar {...props}/> }>
+          <Tab.Screen name="Home" component={Home}/>
+          <Tab.Screen name="Grid" component={Grid}/>
+          <Tab.Screen name="Stats" component={Stats}/>
+        </Tab.Navigator>
+      </NavigationContainer>
+    </NativeBaseProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
