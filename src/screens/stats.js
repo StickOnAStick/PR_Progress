8a69afc8e7edefead9d849f55d0b5c9fc3502ce9
Nicholas T Dimidik <nicholas.dimidik@sjsu.edu>
2022-06-16 20:51:18 -0700

    Added date time / async storage

--- src/screens/stats.js
+++ src/screens/stats.js
@@ -1,20 +1,73 @@
-import { Box, Input, Text, useColorModeValue, useTheme } from "native-base";
+import { Box, Button, Input, Text, useColorModeValue, useTheme } from "native-base";
 import { LineChart } from "react-native-chart-kit";
-import { Dimensions } from "react-native";
+import { Dimensions, View } from "react-native";
 import React, { useState } from 'react';
-
-
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import DateTimePicker from '@react-native-community/datetimepicker';
 
 const Stats = () => {
+    //Storage
+    
+    const storeData = async (value) => {
+        try {
+            
+            await AsyncStorage.setItem('@value_key', value)
+        } catch (e) {
+            console.log("Error in storing data")
+        }
+    }
 
+    const getData = async () => {
+        try{
+            const value = await AsyncStorage.getItem('@value_key')
+            if(value !== null) {
+                return setText(value)
+            }
+        } catch (e){
+            console.log("Error reading value")
+        }
+    }
+
+    
+    //States
     const { colors } = useTheme();
     const [text, setText ] = useState('Default');
+    const [date, setDate] = useState(new Date(Date.now()));
+    const [mode, setMode] = useState('date');
+    const [show, setShow] = useState(false);
 
+    const onChange = (event, selectedDate) => {
+        setDate(selectedDate);
+        setShow(false);
+        console.log(date.toLocaleDateString());
+        console.log(date.getDate());
+        console.log(dayNames[date.getDay()]);
+    };
+
+    const showMode = (currentMode) => {
+        setShow(true);
+        setMode(currentMode);
+    };
+
+    const showDatepicker = () => {
+        showMode('date');
+    };
+
+    const showTimepicker = () => {
+        showMode('time');
+    };
+    
     const handleChange = text => {
         if(!text){setText('Default');}
         else{setText(text);}
         
     };
+
+    const dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+   
+    const getLabels = () => {
+        date.getDay()
+    }; 
         
    
 
@@ -67,7 +120,28 @@ const Stats = () => {
 
             <Input w="100%" size={5} color="muted.400" 
                    onChangeText={handleChange} placeholder="Weight"/>
-            <Text style={{color: 'white', height: 400}}>{text}</Text>
+            <Text style={{color: 'black', height: 50}}>{text}</Text>
+
+            <Input size={5} color="muted.200" onChangeText={storeData} placeholder="store Data"/>
+            <Button onPress={getData} >Get Data</Button>
+            <Text style={{color: 'black', height: 40}}>{text}</Text>
+            <View>
+                <Button onPress={showDatepicker} placeholder="ShowDatePicker">ShowDatePicker</Button>
+                <Button  onPress={showTimepicker} placeholder="showTimePicker">ShowTimePicker</Button>
+
+                {show && (
+                    <DateTimePicker 
+                        testID="dateTimePicker"
+                        value={date}
+                        mode={mode}
+                        is24Hour={true}
+                        display="default"
+                        onChange={onChange}
+                    />
+                )}
+                
+            </View>
+
         </Box>
     );
 };
